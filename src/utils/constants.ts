dcb71702aed8dc9a1d80b5d31e4dd68eb834322c
Oghenero Ologe <59165938+Nero2005@users.noreply.github.com>
2022-08-09 11:48:14 +0100

    Added login page

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,5 @@
-export const API_URL = "https://bellyfood-backend.herokuapp.com/api/v1/";
+const env = process.env.NODE_ENV;
+export const API_URL =
+  env == "production"
+    ? "https://bellyfood-backend.herokuapp.com/api/v1/"
+    : "http://localhost:8000/api/v1/";
