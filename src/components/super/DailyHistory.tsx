898ed96cdad321cbeb26df9c559cc6d7a66f593e
Oghenero Ologe <nologe37@gmail.com>
2022-08-21 11:56:06 +0100

    Refactored history details, added donate section, refactored customer dashboard design

--- src/components/super/DailyHistory.tsx
+++ src/components/super/DailyHistory.tsx
@@ -17,58 +17,18 @@ function DailyHistory({ day, historyDetails }: Props) {
   const [openPayments, setOpenPayments] = useState(false);
   const [openCustomers, setOpenCustomers] = useState(false);
 
-  useEffect(() => {
-    // historyDetails?.histories.forEach(async (historyD, index, historiesA) => {
-    //   const agentDetails = await getAdminByCode(historyD?.agentCode);
-    //   const customerDetails = await getCustomer(historyD?.customerId);
-    //   console.log(agentDetails);
-    //   console.log(customerDetails);
-    //   historiesA[index] = {
-    //     ...historiesA[index],
-    //     agentName: agentDetails.name,
-    //     customerName: customerDetails.name,
-    //   };
-    // });
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className="flex flex-col flex-1 items-center space-y-2">
       <h1 className="text-2xl font-bold">Daily History</h1>
       <div
-        className="flex space-x-6 cursor-pointer"
+        className="flex flex-col md:flex-row items-center space-x-6 cursor-pointer"
         onClick={() => setOpenPayments((prev) => !prev)}
       >
-        <span>Total revenue on {new Date(day).toDateString()}:</span>
-        <span> ₦{historyDetails?.totalAmount}</span>
+        <span>Total revenue on {new Date(day).toDateString()}</span>
+        <span>₦{historyDetails?.totalAmount}</span>
       </div>
-      {/* {historyDetails?.totalAmount !== 0 && (
-        <div
-          className={`${
-            openPayments ? "flex" : "hidden"
-          } overflow-scroll max-w-sm sm:max-w-2xl scrollbar-hide mx-2 border`}
-        >
-          <table className="mx-auto text-center overflow-scroll">
-            <thead>
-              <tr>
-                <th>Agent Name</th>
-                <th>Location</th>
-                <th>Customer Name</th>
-                <th>Amount paid</th>
-              </tr>
-            </thead>
-            <tbody>
-              {agentWorks?.map((agentWork: any, index: any) => (
-                <PaymentAgentHistories
-                  key={index}
-                  histories={agentWork.histories?.filter(
-                    (historyDetails: any) => historyDetails.type === "payment"
-                  )}
-                />
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )} */}
       {historyDetails?.totalAmount > 0 && (
         <PaymentHistories
           openPayments={openPayments}
@@ -78,11 +38,11 @@ function DailyHistory({ day, historyDetails }: Props) {
         />
       )}
       <div
-        className="flex space-x-6 cursor-pointer"
+        className="flex flex-col md:flex-row space-x-6 items-center cursor-pointer mx-2"
         onClick={() => setOpenCustomers((prev) => !prev)}
       >
-        <span>Total customers added on {new Date(day).toDateString()}:</span>
-        <span> {historyDetails?.numNewCustomer}</span>
+        <span>Total customers added on {new Date(day).toDateString()}</span>
+        <span>{historyDetails?.numNewCustomer}</span>
       </div>
       {historyDetails?.numNewCustomer > 0 && (
         <CustomerHistories
@@ -92,9 +52,9 @@ function DailyHistory({ day, historyDetails }: Props) {
           )}
         />
       )}
-      <div className="flex space-x-6">
-        <span>Total deliveries made on {new Date(day).toDateString()}:</span>
-        <span> {historyDetails?.numNewDelivery}</span>
+      <div className="flex flex-col md:flex-row items-center space-x-6 mx-2">
+        <span>Total deliveries made on {new Date(day).toDateString()}</span>
+        <span>{historyDetails?.numNewDelivery}</span>
       </div>
     </div>
   );
