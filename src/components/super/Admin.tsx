1436188b8f8a14cec597c2d693add6841a6aebe9
Oghenero Ologe <nologe37@gmail.com>
2022-09-02 12:47:32 +0100

    added change admin password

--- src/components/super/Admin.tsx
+++ src/components/super/Admin.tsx
@@ -1,7 +1,12 @@
 import { TrashIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
-import { deleteAdmin, disableAdmin, enableAdmin } from "../../services";
+import {
+  changeAdminPassword,
+  deleteAdmin,
+  disableAdmin,
+  enableAdmin,
+} from "../../services";
 import { UserState } from "../../store/userReducer";
 
 interface Props {
@@ -10,13 +15,31 @@ interface Props {
 }
 
 function Admin({ admin, loadAdmins }: Props) {
+  const [adminPassword, setAdminPassword] = useState<any>({});
   const deleteAdminClick = async () => {
     toast.success("Admin deleted, click the refresh icon");
     const data = await deleteAdmin(admin.agentCode!);
   };
+  const changePassword = async (agentId: string) => {
+    const n = toast.loading("Changing password");
+    try {
+      if (!adminPassword.password || adminPassword.password === "") {
+        toast.error("Password required", { id: n });
+        return;
+      }
+      console.log(adminPassword);
+
+      await changeAdminPassword(adminPassword.agentId, adminPassword.password);
+      toast.success("Password changed", { id: n });
+    } catch (err: any) {
+      toast.error("An error occurred: " + err.msg);
+    } finally {
+      setAdminPassword({});
+    }
+  };
   return (
-    <div className="flex flex-col cursor-pointer">
-      <div className="flex space-x-8 md:space-x-32 justify-center py-3 items-center bg-white">
+    <div className="flex flex-col cursor-pointer bg-white">
+      <div className="flex space-x-8 md:space-x-32 justify-center py-3 items-center">
         <h1>{admin.name}</h1>
         <h2>{admin.phone}</h2>
         {admin.approved ? (
@@ -44,6 +67,30 @@ function Admin({ admin, loadAdmins }: Props) {
           className="w-6 h-6 cursor-pointer text-gray-500"
           onClick={async () => await deleteAdminClick()}
         />
+      </div>
+      <div className="flex space-x-2 items-center py-2 justify-center">
+        <input
+          id="password"
+          type="text"
+          placeholder="Enter new password"
+          value={
+            adminPassword.adminId === admin._id ? adminPassword.password : ""
+          }
+          onChange={(e) =>
+            setAdminPassword({
+              adminId: admin._id,
+              password: e.target.value,
+            })
+          }
+          className="px-1 py-1 focus:ring ring-green-400 rounded outline-none bg-gray-200"
+        />
+        <button
+          type="button"
+          className="px-1 py-1 bg-green-300"
+          onClick={() => changePassword(admin._id)}
+        >
+          Change Password
+        </button>
       </div>
     </div>
   );
